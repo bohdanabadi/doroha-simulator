cbb34d6860b5637d35fd46a86acabcf704c692ca
Bogdan <bogdan.abadi22@gmail.com>
2023-06-11 22:25:36 -0400

    Added handling of env variable to decide how to start server and on which port. Added config.yml + appconfig, in addition encapsulated starting the server logic in server.go modified deployment to start app with env variable `ENV=production`

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/bohdanabadi/Traffic-Simulation/api/config"
+	"log"
 )
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "Hi!, Keep your Gin up")
-	})
-	r.RunTLS(":443", "/etc/letsencrypt/live/api.bohdanabadi.com/fullchain.pem", "/etc/letsencrypt/live/api.bohdanabadi.com/privkey.pem")
-
+	cfg := config.LoadConfig()
+	server := config.NewServer(cfg)
+	log.Fatal(server.StartServer())
 }
