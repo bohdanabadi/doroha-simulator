986fe1a6847e697403796eda6e459084f10488ae
Bogdan <bogdan.abadi22@gmail.com>
2023-12-18 22:51:58 -0500

    added database module that includes a dockerfile and pre-requisites .sql files and github action manual deployment

---
+++ database/init.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE SCHEMA IF NOT EXISTS traffic_db;
+--
+-- -- Set the search path to include your schema and public
+-- SET search_path TO traffic_db, public;
+
+CREATE TABLE IF NOT EXISTS traffic_db.journeys (
+                                        id SERIAL PRIMARY KEY,
+                                        starting_point_x FLOAT NOT NULL,
+                                        starting_point_y FLOAT NOT NULL,
+                                        ending_point_x FLOAT NOT NULL,
+                                        ending_point_y FLOAT NOT NULL,
+                                        distance FLOAT NOT NULL,
+                                        date_create TIMESTAMP NOT NULL,
+                                        status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS traffic_db.road_map_points (
+                                               point GEOMETRY NOT NULL
+);
